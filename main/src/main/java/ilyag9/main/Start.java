58ddc1f80ea970bb40153b109b30eafebe7be575
Ilya Golovachev <ilya.golovachev9@gmail.com>
2019-05-30 15:58:32 +0300

    initial commit

---
+++ main/src/main/java/ilyag9/main/Start.java
@@ -0,0 +1,14 @@
+package ilyag9.main;
+
+import org.apache.log4j.Logger;
+
+public class Start {
+
+
+    private static final Logger LOGGER=Logger.getLogger(Start.class);
+
+    public static void main(String[] args) {
+
+        LOGGER.info("Starting...");
+    }
+}
